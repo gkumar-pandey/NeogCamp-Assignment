9bed02564fe61da81dc1386e7abe182d6cd522a0
gkumar-pandey <gautamshekhar078@gmail.com>
2023-02-21 02:06:39 +0530

    solve set-3 1st ques

---
+++ ES6/Set3/Ques1.js
@@ -0,0 +1,14 @@
+// Q.Create a function that takes an array of strings as an argument and returns a string that includes the number of items in the array and the first and last items
+
+const formatArray = (arr) => {
+  let firstEle = arr[0];
+  let lastEle = arr[arr.length - 1];
+  let arrLength = arr.length;
+
+  return `The array has ${arrLength}, and the first item is "${firstEle}", and the last item is "${lastEle}"`;
+};
+
+const items = ["apple", "banana", "orange"];
+const message = formatArray(items);
+console.log(message);
+// The array has 3 items, and the first item is "apple", and the last item is "orange".
