acd002274768a4a74103ab567821cb5604071365
gkumar-pandey <gautamshekhar078@gmail.com>
2023-03-04 23:53:19 +0530

    update

---
+++ Functional Programming/Set2/Ques3.js
@@ -0,0 +1,16 @@
+//?Given employee’s data, write an ES6 function which greets them with a personalized message for onboarding.
+
+const employeeData = [
+  { name: "ram", dept: "marketer" },
+  { name: "Radha", dept: "SDE" },
+  { name: "shyam", dept: "finance professional" }
+];
+
+// Your code here
+
+// const greetEmployeeMessages = employeeData.map(
+//   ({ name }) => `Hi ${name} we are glad to ahve you as a marketing`
+// );
+
+// console.log(greetEmployeeMessages);
+// Output: ['Hi ram we are glad to have you as a marketing', 'Hi Radha we are glad to have you as a SDE', 'Hi shyam we are glad to have you as a finance professional']
