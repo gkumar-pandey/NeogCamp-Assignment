4d297aad74eaa7b1f0a23cffb9c596131365a201
gkumar-pandey <gautamshekhar078@gmail.com>
2023-03-07 23:14:19 +0530

    solve set 3 functional programming

---
+++ Functional Programming/Set3/Ques14.js
@@ -0,0 +1,9 @@
+//?Write an ES6 function that takes an array of strings and returns a single string that is the concatenation of all the strings using the reduce function.
+const strings = ["Hello", " ", "world", "!"];
+// Your code here
+
+const concatenateStrings = (strings) =>
+  strings.reduce((acc, currValue) => acc + currValue, "");
+
+console.log(concatenateStrings(strings));
+// Output: "Hello world!"
---
+++ Functional Programming/Set3/Ques15.js
@@ -0,0 +1,12 @@
+//?Write an ES6 function that takes an array of numbers and returns the minimum number using the reduce function.
+const numbers = [10, 5, 8, 3, 6];
+
+// Your code here
+
+const getMinimumNumber = (numbers) =>
+  numbers.reduce((acc, currValue) => {
+    return;
+  });
+
+console.log(getMinimumNumber(numbers));
+// Output: 3
